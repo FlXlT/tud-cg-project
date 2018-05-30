f8462e0af51062a49680e01008bc87c40d81cecc
Jesse Tilro <jesse@jessetilro.nl>
2018-05-30 11:56:38 +0200

    Add very basic terrain movement.

--- game/terrain.cpp
+++ game/terrain.cpp
@@ -2,14 +2,20 @@
 
 void Terrain::buildGeometry() {
 	body.diffuseColor = { 133.0f / 255.0f, 104.0f / 255.0f, 238.0f / 255.0f };
-	body.generateSurface(surface_modes::sinusoidal, 10, 20, 1.5f, 20, 40, 10, 10, 2, 4);
+	body.generateSurface(surface_modes::sinusoidal, 20, 20, 1.5f, 40, 40, 10, 10, 4, 4);
 	body.specularColor = { 0, 0, 0 };
 	body.specularIntensity = 0;
 	body.useTex = true;
 	updateGeometry();
+	position.y = 5;
+	targetSpeed.y = -0.05f;
 }
 
 void Terrain::updateGeometry() {
+	if (position.y <= 0) {
+		position.y = 5;
+	}
+
 	body.clearModelMatrix();
 	body.position = position;
 	body.applyPosition();
